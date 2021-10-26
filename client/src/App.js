2d260a7493711f46fd1717e92f885d1483be6579
Zhenni <zhenni.qian913@gmail.com>
2021-10-26 23:43:28 +0200

    install tailwind, react-router, eslint (#4)

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import React, { Suspense, lazy } from "react"
+import { Route, Switch } from "react-router-dom"
+
+const Home = lazy(() => import("./pages/Home"))
 
 function App() {
   return (
     <div className="App">
-      boiler plate
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Suspense>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
