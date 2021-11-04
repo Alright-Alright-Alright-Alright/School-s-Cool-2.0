d3d62a454e268e01b0e31e0d1993b0f675f28bb2
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-04 13:34:00 +0100

    created a main layout component

---
+++ client/src/components/layout/main.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/forbid-prop-types */
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function Main({ main, contentLeft, contentRight }) {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 gap-8">
+        <div>{contentLeft}</div>
+        <div className="md:col-start-1 md:col-span-1 lg:col-start-2 lg:col-span-2">
+          {main}
+        </div>
+        <div>{contentRight}</div>
+      </div>
+    </>
+  )
+}
+
+Main.propTypes = {
+  main: PropTypes.any.isRequired,
+  contentLeft: PropTypes.any,
+  contentRight: PropTypes.any,
+}
