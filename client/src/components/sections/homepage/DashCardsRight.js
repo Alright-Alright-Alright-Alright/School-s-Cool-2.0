83a6af0d0c9b924c99ee97ab2777803167ff3a22
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-04 13:34:32 +0100

    created homepage segments for example

---
+++ client/src/components/sections/homepage/DashCardsRight.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function DashCardsRight() {
+  return (
+    <div>
+      <h1>This is content for the right column</h1>
+    </div>
+  )
+}
+
+export default DashCardsRight
