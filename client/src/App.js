1cfcef4f997ebb41dd4e5a7c5edf646b5aab19ae
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-07 11:27:36 +0100

    eslint fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React, { Suspense, lazy } from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { Suspense, lazy } from "react"
+import { Route, Switch } from "react-router-dom"
 
-const Home = lazy(() => import("./pages/Home"));
+const Home = lazy(() => import("./pages/Home"))
 
 function App() {
-	return (
-		<div className="App">
-			<Suspense fallback={<div>Loading...</div>}>
-				<Switch>
-					<Route exact path="/" component={Home} />
-				</Switch>
-			</Suspense>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Suspense>
+    </div>
+  )
 }
 
-export default App;
+export default App
