c7c563e57de11f948f34abe06ef95f14b72583c7
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-08 13:12:16 +0100

    side content now hidden on smaller screens

--- client/src/components/layout/main.js
+++ client/src/components/layout/main.js
@@ -6,12 +6,10 @@ import PropTypes from "prop-types"
 export default function Main({ main, contentLeft, contentRight }) {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-4 bg-grey-super_light h-screen ">
-        <div className="flex-none justify-center">{contentLeft}</div>
-        <div className="md:col-start-1 md:col-span-1 lg:col-start-2 lg:col-span-2">
-          {main}
-        </div>
-        <div className="flex-none justify-center">{contentRight}</div>
+      <div className="grid lg:grid-cols-4 bg-grey-super_light h-screen">
+        <div className="hidden lg:block">{contentLeft}</div>
+        <div className="lg:col-span-2">{main}</div>
+        <div className="hidden lg:block">{contentRight}</div>
       </div>
     </>
   )
