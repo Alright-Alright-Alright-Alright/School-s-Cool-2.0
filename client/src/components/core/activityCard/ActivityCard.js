7d9b0a1d4f7cbe96d99cbfebccf54dc8405a8c43
Dirk Kelderman <66555191+dirkkelderman@users.noreply.github.com>
2021-11-08 20:07:39 +0100

    Activity cards and feed (#16)
    * created intro/top section
    
    * started on activity card
    
    * small changes styling wize

---
+++ client/src/components/core/activityCard/ActivityCard.js
@@ -0,0 +1,63 @@
+import React from "react"
+
+function ActivityCard() {
+  return (
+    <div className="rounded-bl-2xl rounded-br-2xl rounded-r-2xl bg-white shadow-lg m-3">
+      <div className="p-3">
+        <img
+          className="object-cover w-full max-h-40 rounded-bl-2xl rounded-br-2xl rounded-r-2xl"
+          src="https://via.placeholder.com/390x184"
+          alt="banner"
+        />
+      </div>
+      <div>
+        <div className="flex justify-between pl-3 pr-3">
+          <div className="flex items-center">
+            <img
+              className="w-10 h-10 rounded-full mr-2"
+              src="https://via.placeholder.com/40x40"
+              alt="profile"
+            />
+            <p className="text-base">User name</p>
+            <p className="text-base pl-3 text-grey-medium_light">
+              Commented on
+            </p>
+            <a href="/" className="text-base pl-3">
+              Topic name
+            </a>
+          </div>
+          <div className="flex items-center">
+            <p className="text-base">Date</p>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <p className="border-b-2 border-grey-light m-3 pb-3 text-base">
+          Olore de volut audae velitiori ipsuntiate litatur secuptatias iunturi
+          osandeliquis reni as debissit hit ommossequi dis a quid untis quist
+          dolor sandist inciminctet ut officiu ntiam, eliatis anducit alit fuga.
+          Olore de volut audae velitiori ipsuntiate litatur secuptatias iunturi
+          osandeliquis reni as debissit hit ommossequi dis a quid untis quist
+          dolor sandist inciminctet ut officiu ntiam, eliatis anducit alit fuga.
+        </p>
+      </div>
+      <div className="flex justify-end pt-1 pr-3">
+        <p className="text-base">Icons</p>
+      </div>
+      <div className="flex p-3">
+        <img
+          className="w-10 h-10 rounded-full mr-2"
+          src="https://via.placeholder.com/30x30"
+          alt="profile"
+        />
+        <input
+          className="bg-grey-super_light w-full rounded-full text-base pl-3"
+          type="text"
+          placeholder="Add a comment"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ActivityCard
--- client/src/components/sections/homepage/ActivityFeed.js
+++ client/src/components/sections/homepage/ActivityFeed.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
+import ActivityCard from "../../core/activityCard/ActivityCard"
+import Button from "../../core/Button"
 
 function ActivityFeed() {
+  const [filter, setFilter] = useState(true)
+
   return (
     <div className="mt-3">
-      <h1 className="text-xl">Activity Feed</h1>
+      <div className="sm:flex md:place-content-between">
+        <div>
+          <h1 className="text-lg text-center mb-3 md:mb-0 pl-3">
+            Activity Feed
+          </h1>
+        </div>
+        <div className="space-x-4 md:place-content-between text-center pr-3">
+          <Button
+            buttonName="All activity"
+            btnPrimary={filter}
+            onClick={() => setFilter(!filter)}
+          />
+          <Button
+            buttonName="Followed activity"
+            btnPrimary={!filter}
+            onClick={() => setFilter(!filter)}
+          />
+        </div>
+      </div>
+      <ActivityCard />
+      <ActivityCard />
+      <ActivityCard />
     </div>
   )
 }
