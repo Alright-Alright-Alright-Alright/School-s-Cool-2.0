62896f680809a8b805e11917161b28f05a4e2f12
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-12 10:04:06 +0100

    made some redux changes to return array instead of object

--- client/src/redux/actions/topicActions.js
+++ client/src/redux/actions/topicActions.js
@@ -1,13 +1,30 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios"
 import { SET_TOPICS } from "../types/topics"
+import TopicService from "../services/topicService"
 
 import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types/ui"
+
+const service = new TopicService()
 
 export const getAllTopics = () => (dispatch) => {
   dispatch({ type: LOADING_UI })
 
+  // service
+  //   .getAllTopics()
+  //   .then((response) => {
+  //     dispatch({ type: CLEAR_ERRORS })
+  //     dispatch({ type: SET_TOPICS, payload: response.data })
+  //   })
+  //   .catch((err) => {
+  //     dispatch({
+  //       type: SET_ERRORS,
+  //       payload: err.response,
+  //     })
+  //   })
+
   axios
-    .get("http://localhost:5001/api/topics")
+    .get("http://localhost:5000/api/topics")
     .then((response) => {
       dispatch({ type: CLEAR_ERRORS })
       dispatch({ type: SET_TOPICS, payload: response.data })
--- client/src/redux/reducers/topicReducer.js
+++ client/src/redux/reducers/topicReducer.js
@@ -1,16 +1,14 @@
+/* eslint-disable no-unused-vars */
 import { SET_TOPICS } from "../types/topics"
 
 const initialState = {
   topics: [],
 }
 
-const topicReducer = (state = initialState, action) => {
+const topicReducer = (state = initialState.topics, action) => {
   switch (action.type) {
     case SET_TOPICS:
-      return {
-        ...state,
-        ...action.payload,
-      }
+      return [...state, ...action.payload]
     default:
       return state
   }
