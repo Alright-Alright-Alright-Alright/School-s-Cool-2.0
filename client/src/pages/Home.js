eb7e1e7726a62a1ea6a6362bc12ec601fb175373
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-14 09:19:13 +0100

    created router paths for login, register and home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,12 @@
 import React from "react"
 import NavBar from "../components/layout/NavBar"
-// import Index from "../components/sections/register/Index"
-// import RegisterIndex from "../components/sections/register/RegisterIndex"
-// import TopicsTest from "./topicsTest/topicsTest"
-import LoginIndex from "../components/sections/login/LoginIndex"
+import Index from "../components/sections/homepage/Index"
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      {/* <RegisterIndex /> */}
-      <LoginIndex />
+      <Index />
     </>
   )
 }
---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React from "react"
+import RegisterIndex from "../components/sections/register/RegisterIndex"
+
+function Register() {
+  return (
+    <>
+      <RegisterIndex />
+    </>
+  )
+}
+
+export default Register
