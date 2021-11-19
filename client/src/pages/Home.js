3b665e49313eec8f57cd9a17fd8e512ba7d98931
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-19 12:50:38 +0100

    deleted navbar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from "react"
-import NavBar from "../components/layout/NavBar"
 import Index from "../components/sections/homepage/Index"
 
 export default function Home() {
   return (
     <>
-      <NavBar />
       <Index />
     </>
   )
