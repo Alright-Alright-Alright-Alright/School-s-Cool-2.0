1a07ac8532fe213a9141f07205a2396d210e53ca
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-19 12:49:55 +0100

    configured the new react router way

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
+/* eslint-disable no-unused-vars */
 import React, { Suspense, lazy } from "react"
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, Outlet } from "react-router-dom"
+import NavBar from "./components/layout/NavBar"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import TopicDetailPage from "./pages/TopicDetailPage"
+import Home from "./pages/Home"
 
-const Home = lazy(() => import("./pages/Home"))
+// const Home = lazy(() => import("./pages/Home"))
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
+      <NavBar />
+      {/* <Home /> */}
+      <Outlet />
+      {/* <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
+          <Route path="/topics" element={<TopicDetailPage />}>
+            <Route path=":topicId" element={<TopicDetailPage />} />
+          </Route>
         </Routes>
-      </Suspense>
+      </Suspense> */}
     </div>
   )
 }
