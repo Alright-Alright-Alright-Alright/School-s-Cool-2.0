4215eae36beec20dc8c85ff078cb77f5d3859441
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-20 13:48:57 +0100

    Add functionallities for post redux

---
+++ client/src/redux/types/posts.js
@@ -0,0 +1,7 @@
+// Posts reducer types
+export const SET_POSTS = "SET_POSTS"
+export const POST_POST = "POST_POST"
+export const DELETE_POST = "DELETE_POST"
+export const LIKE_POSTS = "LIKE_POST"
+export const UNLIKE_POSTS = "UNLIKE_POST"
+export const SUBMIT_COMMENT = "SUBMIT_COMMENT"
