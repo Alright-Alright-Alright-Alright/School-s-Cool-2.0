0355f0a3e3feaf2488044d3bec2fd71135f6535f
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-29 20:44:28 +0100

    Added comment routes and updated post

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -15,11 +15,20 @@ const getAllComments = async (req, res, next) => {
 };
 
 const createComment = async (req, res, next) => {
+<<<<<<< HEAD
   const { commentBody, owner } = req.body;
   const { postId } = req.params
 
   try {
     const comment = await createCommentService(commentBody, owner, postId);
+=======
+  const { body, owner } = req.body;
+  const { postId } = req.params
+
+  // const { userId } = req.userLogedIn._id
+  try {
+    const comment = await createCommentService(body, owner, postId);
+>>>>>>> f5f63eb (Added comment routes and updated post)
     return res.status(201).json({ message: "Success", comment });
   } catch (e) {
     res.status(500).json({ message: e.message }) && next(e);
