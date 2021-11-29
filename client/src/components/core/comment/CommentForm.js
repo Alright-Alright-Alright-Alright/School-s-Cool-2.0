e381aeaa3392fc4363715e98e153b095f12869ce
Dirk Kelderman <dirkkelderman@gmail.com>
2021-11-29 20:42:55 +0100

    Added dynamic data from db

--- client/src/components/core/comment/CommentForm.js
+++ client/src/components/core/comment/CommentForm.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 function CommentForm() {
+  const user = useSelector((state) => state.user)
+
   return (
     <form className="flex px-5 pb-5 pt-3">
       <img
         className="w-10 h-10 rounded-full mr-2"
-        src="https://via.placeholder.com/30x30"
+        src={`${user?.imageUrl}`}
         alt="profile"
       />
       <input
