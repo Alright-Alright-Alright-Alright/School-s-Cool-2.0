12a2c47ecf78d0b62b10fd3e87cd03d0f7821abd
Dirk Kelderman <dirkkelderman@gmail.com>
2021-12-02 09:15:17 +0100

    Added dropdown data, topicdashcard and refactoring

--- client/src/components/core/topicDashCard/TopicDashCardListItem.js
+++ client/src/components/core/topicDashCard/TopicDashCardListItem.js
@@ -1,52 +1,36 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/require-default-props */
+/* eslint-disable  */
 /* eslint-disable react/forbid-prop-types */
 import React from "react"
 import PropTypes from "prop-types"
-import { Link } from "react-router-dom"
 import Icon from "../Icon"
 
 export default function TopicDashCardListItem({
   listItemFirstName,
   listItemLastName,
   topicDashCardData,
-
-  // listItemTitle,
-  // listItemDate,
-  // listItemComments,
-  // listItemUsers,
 }) {
-  console.log(topicDashCardData)
   return (
     <>
       <div className="pt-3 flex justify-between">
         <div className="flex-col">
-          {/* <Link to="/">{listItemTitle}</Link>
-          {listItemDate && (
-            <p className="text-xs text-grey-medium pb-1">{listItemDate}</p>
-          )} */}
           <p>
             {listItemFirstName} {listItemLastName}
           </p>
         </div>
-        <div className="flex gap-1 w-20 grid-cols-2 pb-1 items-center">
-          <div className="flex gap-1 w-20">
-            {topicDashCardData?.includes(listItemFirstName) ? (
-              <p>yes</p>
+        <div className="flex w-20 pb-1 justify-end">
+          <div className="flex ">
+            {topicDashCardData?.some(
+              (user) => user.firstName === listItemFirstName
+            ) ? (
+              <button type="button">
+                <Icon iconName="follow" iconStyle="fill-active text-grey-dark" />
+              </button>
             ) : (
-              // <Icon iconName="add" iconStyle="fill-active text-grey-dark" />
-              <p>no</p>
-              // <Icon iconName="add" iconStyle="fill-inactive text-grey-dark" />
+              <button type="button">
+                <Icon iconName="add" iconStyle="fill-inactive text-grey-dark" />
+              </button>
             )}
-            {/* <button type="button">
-              <Icon iconName="add" iconStyle="fill-inactive text-grey-dark" />
-            </button> */}
-            {/* <p>{listItemComments?.length}</p> */}
           </div>
-          <button type="button">
-            <Icon iconName="follow" iconStyle="fill-inactive text-grey-dark" />
-          </button>
-          {/* <p>{listItemUsers?.length}</p> */}
         </div>
       </div>
     </>
@@ -57,8 +41,5 @@ TopicDashCardListItem.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   listItemFirstName: PropTypes.string.isRequired,
   listItemLastName: PropTypes.string.isRequired,
-  topicDashCardData: PropTypes.object.isRequired,
-  // listItemDate: PropTypes.string,
-  // listItemComments: PropTypes.array.isRequired,
-  // listItemUsers: PropTypes.array.isRequired,
+  topicDashCardData: PropTypes.array.isRequired,
 }
--- client/src/components/core/topicDashCard/TopicDashcard.js
+++ client/src/components/core/topicDashCard/TopicDashcard.js
@@ -2,44 +2,40 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/default-props-match-prop-types */
 /* eslint-disable react/require-default-props */
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable  */
+
+
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import Icon from "../Icon"
-import DashCardListItem from "./TopicDashCardListItem"
+import TopicDashCardListItem from "./TopicDashCardListItem"
 import DropDownMenu from "../DropDownMenu"
 
 export default function TopicDashcard({
   topicDashCardData,
   topicDashCardTitle,
   topicDashCardStyle,
+  dropdownMenuData,
+  users,
 }) {
   const [expandDashCard, setExpandDashCard] = useState(false)
 
-  console.log(topicDashCardData)
-
-  const firstThreeItems = topicDashCardData.slice(0, 3).map((item) => (
-    <DashCardListItem
-      key={item._id}
-      listItemFirstName={item?.firstName}
-      listItemLastName={item?.lastName}
-      topicDashCardData={topicDashCardData}
-
-      // listItemDate={item?.date}
-      // listItemComments={item?.comment}
-      // listItemUsers={item?.user}
-    />
-  ))
-
-  const allItems = topicDashCardData.map((item) => (
-    <DashCardListItem
+  const firstThreeItems = users.slice(0, 3).map((item) => (
+    <TopicDashCardListItem
       key={item?._id}
       listItemFirstName={item?.firstName}
       listItemLastName={item?.lastName}
       topicDashCardData={topicDashCardData}
+    />
+  ))
 
-      // listItemDate={item?.date}
-      // listItemComments={item?.comment}
-      // listItemUsers={item?.user}
+  const allItems = users.map((item) => (
+    <TopicDashCardListItem
+      key={item?._id}
+      listItemFirstName={item?.firstName}
+      listItemLastName={item?.lastName}
+      topicDashCardData={topicDashCardData}
     />
   ))
 
@@ -53,7 +49,7 @@ export default function TopicDashcard({
             <p className="text-lg pl-4">{topicDashCardTitle}</p>
             <div className="flex flex-row">
               <h2 className="text-base pr-4">Filter</h2>
-              <DropDownMenu />
+              <DropDownMenu data={dropdownMenuData} />
             </div>
           </div>
         </div>
@@ -82,11 +78,12 @@ export default function TopicDashcard({
 
 TopicDashcard.defaultProps = {
   topicDashCardData: [],
+  users: [],
 }
 
 TopicDashcard.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
   topicDashCardData: PropTypes.array.isRequired,
   topicDashCardTitle: PropTypes.string.isRequired,
   topicDashCardStyle: PropTypes.string.isRequired,
+  users: PropTypes.array.isRequired,
 }
