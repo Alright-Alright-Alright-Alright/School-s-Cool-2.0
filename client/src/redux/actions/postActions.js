84a6c89ce1e63098f83625828185ddf161fc1d23
Dirk Kelderman <dirkkelderman@gmail.com>
2021-12-03 21:09:14 +0100

    refactor post redux

--- client/src/redux/actions/postActions.js
+++ client/src/redux/actions/postActions.js
@@ -1,164 +1,81 @@
 /* eslint-disable no-unused-vars */
 import axios from "axios"
 import { SET_POSTS, GET_POST, SET_POST, SUBMIT_COMMENT } from "../types/posts"
-import { getPostByIdService } from "../services/postService"
+import {
+  getPostByIdService,
+  createPostService,
+  submitCommentService,
+} from "../services/postService"
 import { SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types/ui"
 
 const authToken = localStorage.getItem("Authorization")
 
-// export const getAllPosts = (topicId) => (dispatch) => {
-//   dispatch({ type: LOADING_UI })
-
-//   axios
-//     .get(`${process.env.REACT_APP_API_URL}/topics/${topicId}/posts`, topicId, {
-//       headers: { Authorization: authToken },
-//     })
-//     .then((response) => {
-//       dispatch({ type: CLEAR_ERRORS })
-//       dispatch({ type: SET_POSTS, payload: response.data })
-//     })
-//     .catch((err) => {
-//       dispatch({
-//         type: SET_ERRORS,
-//         payload: err.response.data,
-//       })
-//     })
-// }
-
-export const createPost = (newPost) => (dispatch) => {
+export const getPostById = (postId) => async (dispatch) => {
   dispatch({ type: LOADING_UI })
-  axios
-    .post(
-      `${process.env.REACT_APP_API_URL}/topics/${newPost.topicId}/posts`,
-      newPost,
-      {
-        headers: { Authorization: authToken },
-      }
+
+  const postById = await getPostByIdService(postId)
+  try {
+    dispatch({ type: CLEAR_ERRORS })
+    dispatch({ type: GET_POST, payload: postById })
+  } catch (error) {
+    dispatch({
+      type: SET_ERRORS,
+      payload: error.response,
+    })
+  }
+}
+
+export const createPost = (newPost) => async (dispatch) => {
+  dispatch({ type: LOADING_UI })
+
+  const addNewPostDb = await createPostService(newPost)
+  try {
+    dispatch({ type: CLEAR_ERRORS })
+    dispatch({ type: SET_POST, payload: addNewPostDb })
+  } catch (error) {
+    dispatch({
+      type: SET_ERRORS,
+      payload: error.response,
+    })
+  }
+}
+
+export const submitComment =
+  (owner, commentBody, postId) => async (dispatch) => {
+    dispatch({ type: LOADING_UI })
+
+    const addNewCommentDb = await submitCommentService(
+      owner,
+      commentBody,
+      postId
     )
-    .then((response) => {
+    console.log(addNewCommentDb)
+    try {
       dispatch({ type: CLEAR_ERRORS })
-      dispatch({ type: SET_POST, payload: response.data })
-    })
-    .catch((err) => {
+      dispatch({ type: SUBMIT_COMMENT, payload: addNewCommentDb.comment })
+    } catch (error) {
       dispatch({
         type: SET_ERRORS,
-        payload: err.response.data,
+        payload: error.response,
       })
-    })
-}
+    }
 
-export const getPostById = (postId) => (dispatch) => {
-  dispatch({ type: LOADING_UI })
-  axios
-    .get(`${process.env.REACT_APP_API_URL}/posts/${postId}`, {
-      headers: { Authorization: authToken },
-    })
-    .then((response) => {
-      dispatch({ type: CLEAR_ERRORS })
-      dispatch({ type: GET_POST, payload: response.data })
-    })
-    .catch((err) => {
-      dispatch({
-        type: SET_ERRORS,
-        payload: err.response.data,
-      })
-    })
-}
-
-// export const getPostById = (postId) => async (dispatch) => {
-//   dispatch({ type: LOADING_UI })
-
-//   const postFromDB = await getPostByIdService(postId)
-//   console.log(postFromDB)
-
-//   try {
-//     dispatch({ type: CLEAR_ERRORS })
-//     dispatch({ type: GET_POST, payload: postFromDB })
-//   } catch (error) {
-//     dispatch({
-//       type: SET_ERRORS,
-//       payload: error.response,
-//     })
-//   }
-// }
-
-export const submitComment = (owner, commentBody, postId) => (dispatch) => {
-  console.log(postId, commentBody, owner)
-  dispatch({ type: LOADING_UI })
-  axios
-    .post(
-      `${process.env.REACT_APP_API_URL}/posts/${postId}/comments`,
-      { commentBody, owner },
-      {
-        headers: { Authorization: authToken },
-      }
-    )
-    .then((response) => {
-      dispatch({ type: CLEAR_ERRORS })
-      dispatch({ type: SET_POST, payload: response.data })
-    })
-    .catch((err) => {
-      dispatch({
-        type: SET_ERRORS,
-        payload: err.response.data,
-      })
-    })
-}
-
-// export const getPostById = (postId) => (dispatch) => {
-//   dispatch({ type: LOADING_UI })
-//   axios
-//     .get(`${process.env.REACT_APP_API_URL}/posts/${postId}`, {
-//       headers: { Authorization: authToken },
-//     })
-//     .then((response) => {
-//       dispatch({ type: CLEAR_ERRORS })
-//       dispatch({ type: GET_POST, payload: response.data })
-//     })
-//     .catch((err) => {
-//       dispatch({
-//         type: SET_ERRORS,
-//         payload: err.response.data,
-//       })
-//     })
-// }
-
-// export const getPostById = (postId) => async (dispatch) => {
-//   dispatch({ type: LOADING_UI })
-
-//   const postFromDB = await getPostByIdService(postId)
-//   console.log(postFromDB)
-
-//   try {
-//     dispatch({ type: CLEAR_ERRORS })
-//     dispatch({ type: GET_POST, payload: postFromDB })
-//   } catch (error) {
-//     dispatch({
-//       type: SET_ERRORS,
-//       payload: error.response,
-//     })
-//   }
-// }
-
-// export const submitComment = (owner, commentBody, postId) => (dispatch) => {
-//   console.log(postId, commentBody, owner)
-//   dispatch({ type: LOADING_UI })
-//   axios
-//     .post(
-//       `${process.env.REACT_APP_API_URL}/posts/${postId}/comments`,
-//       { commentBody, owner },
-//       {
-//         headers: { Authorization: authToken },
-//       }
-//     )
-//     .then((response) => {
-//       dispatch({ type: CLEAR_ERRORS })
-//       dispatch({ type: SUBMIT_COMMENT, payload: response.data })
-//     })
-//     .catch((err) => {
-//       dispatch({
-//         type: SET_ERRORS,
-//         payload: err.response.data,
-//       })
-//     })
-// }
+    // axios
+    //   .post(
+    //     `${process.env.REACT_APP_API_URL}/posts/${postId}/comments`,
+    //     { commentBody, owner },
+    //     {
+    //       headers: { Authorization: authToken },
+    //     }
+    //   )
+    //   .then((response) => {
+    //     dispatch({ type: CLEAR_ERRORS })
+    //     dispatch({ type: SET_POST, payload: response.data })
+    //   })
+    //   .catch((err) => {
+    //     dispatch({
+    //       type: SET_ERRORS,
+    //       payload: err.response.data,
+    //     })
+    //   })
+  }
