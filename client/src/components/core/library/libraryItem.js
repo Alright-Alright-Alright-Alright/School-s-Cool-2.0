643660d3ee31f94e415f0d5731f414e00de5a106
In-S4n3 <tiagodcpp@gmail.com>
2021-12-09 17:52:33 +0000

    formating the table

--- client/src/components/core/library/libraryItem.js
+++ client/src/components/core/library/libraryItem.js
@@ -2,10 +2,11 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
 import React from "react"
+import Icon from "../Icon"
 
 const libraryItem = ({ library }) => (
   <div className="flex justify-center content-center bg- w-full">
-    <table className="w-full table-fixed border-solid border-2">
+    <table className="w-full border-solid border-2">
       <thead>
         <tr className="border-solid border-2">
           <th className="border-solid border-2">Name</th>
@@ -15,9 +16,12 @@ const libraryItem = ({ library }) => (
         </tr>
       </thead>
       {library.map((item) => (
-        <tbody className="border-solid border-2">
-          <tr key={item._id}>
-            <td>{item.fileName}</td>
+        <tbody key={item._id} className="border-solid border-2">
+          <tr className="h-16">
+            <td className="flex flex-col h-16">
+              <span>{item.fileName}</span>
+              <span> {item.subject}</span>
+            </td>
             <td>{item.category}</td>
             <td>
               {JSON.stringify(
@@ -26,8 +30,12 @@ const libraryItem = ({ library }) => (
                 }/${new Date().getFullYear()}`
               )}
             </td>
-            <td>
-              {item.owner.firstName} {item.owner.lastName}
+            <td className="h-16 flex justify-evenly content-center">
+              <span>
+                {item.owner.firstName} {item.owner.lastName}
+              </span>
+              <Icon iconName="like" iconStyle="fill-inactive" />
+              <Icon iconName="message" iconStyle="fill-inactive" />
             </td>
           </tr>
         </tbody>
