431559887f91b70c609d90f46acd11c17abed99b
Dirk Kelderman <dirkkelderman@gmail.com>
2021-12-09 16:39:27 +0100

    Added resource dashcard

---
+++ client/src/components/core/resourceDashCard/ResourceDashCardListItem.js
@@ -0,0 +1,45 @@
+/* eslint-disable  */
+/* eslint-disable react/forbid-prop-types */
+import React from "react"
+import PropTypes from "prop-types"
+import Icon from "../Icon"
+import { Link } from "react-router-dom"
+
+export default function TopicDashCardListItem({
+  listItemName,
+  listItemLikes,
+  listItemComments,
+  topicDashCardData,
+}) {
+  return (
+    <>
+    <div className="pt-3 flex justify-between">
+      <div className="flex-col">
+        <Link to={`/topics/${listItemName}`}>{listItemName}</Link>
+      </div>
+      <div className="flex gap-1 w-20 grid-cols-2 pb-1 items-center">
+        <div className="flex gap-1 w-20">
+          <button type="button">
+            <Icon
+              iconName="like"
+              iconStyle="fill-inactive text-grey-dark"
+            />
+          </button>
+          <p>{listItemComments?.length}</p>
+        </div>
+        <button type="button">
+          <Icon iconName="message" iconStyle="fill-inactive text-grey-dark" />
+        </button>
+        <p>{listItemLikes?.length}</p>
+      </div>
+    </div>
+  </>
+  )
+}
+
+TopicDashCardListItem.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  listItemFirstName: PropTypes.string.isRequired,
+  listItemLastName: PropTypes.string.isRequired,
+  topicDashCardData: PropTypes.array.isRequired,
+}
