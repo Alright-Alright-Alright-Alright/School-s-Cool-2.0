5fb3a46acedd22bffbeb07e3963e2e3d819a095b
In-S4n3 <tiagodcpp@gmail.com>
2021-12-19 12:12:25 +0000

    Event modal in progress

--- client/src/components/core/events/EventModal.js
+++ client/src/components/core/events/EventModal.js
@@ -3,19 +3,23 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useRef } from "react"
 import { useDispatch } from "react-redux"
+import DatePicker from "react-datepicker"
 import fileUploadHandler from "../../../middleware/UploadFile"
+import { addAtopic, getAlltopics } from "../../../redux/actions/topicActions"
 import Button from "../Button"
 import SwitchButton from "../SwitchButton"
 import Icon from "../Icon"
-import { addFileToLibrary } from "../../../redux/actions/libraryActions"
+import "react-datepicker/dist/react-datepicker.css"
 
-const Modal = ({ handleShowModal, singleTopic }) => {
+const Modal = ({ handleShowModal }) => {
   const [title, seTitle] = useState("")
-  const [category, setCategory] = useState("")
-  const [subject, setSubject] = useState("")
-  const [fileUrl, setFilUrl] = useState("")
-  const [imgPreview, setImgPreview] = useState("")
+  const [location, setLocation] = useState("")
+  const [description, setDescription] = useState("")
+  const [bannerImage, setBannerImage] = useState("")
   const [privacy, setPrivacy] = useState(false)
+  const [dateRange, setDateRange] = useState([null, null])
+  const [startDate, endDate] = dateRange
+
   const hiddenFileInput = useRef(null)
 
   const dispatch = useDispatch()
@@ -24,108 +28,111 @@ const Modal = ({ handleShowModal, singleTopic }) => {
     hiddenFileInput.current.click()
   }
 
-  const handleImagePreview = async (img) => {
-    const image = await fileUploadHandler(img)
-    setImgPreview(image)
-  }
-
   const chooseTitle = (e) => {
     seTitle(e.target.value)
   }
 
-  const chooseBannerImage = async (e) => {
-    setFilUrl(e.target.files[0])
-    handleImagePreview(e.target.files[0])
+  const chooseDescription = (e) => {
+    setDescription(e.target.value)
   }
 
-  const chooseCategory = (e) => {
-    setCategory(e.target.value)
+  const chooseBannerImage = (e) => {
+    setBannerImage(e.target.files[0])
   }
 
-  const chooseSubject = (e) => {
-    setSubject(e.target.value)
+  const chooseLocation = (e) => {
+    setLocation(e.target.value)
   }
 
   const handleFormSubmit = async (e) => {
     e.preventDefault()
-    const image = await fileUploadHandler(fileUrl)
-    const fileData = {
+    const image = await fileUploadHandler(bannerImage)
+    const topicData = {
       title,
-      category: singleTopic ? singleTopic.category : category,
-      subject: singleTopic ? singleTopic.subject : subject,
-      fileUrl: image,
+      description,
+      dateStart: startDate,
+      dateEnd: endDate,
+      location,
+      bannerImage: image,
       isPrivate: privacy,
     }
     handleShowModal()
-    dispatch(addFileToLibrary(fileData))
+    dispatch(addAtopic(topicData))
+    dispatch(getAlltopics())
   }
 
   return (
-    <div className="absolute inset-0 flex justify-center pt-28 z-50">
-      <form
-        className="h-72 w-6/8 rounded-2xl bg-white flex flex-col justify-evenly shadow-2xl"
-        onSubmit={handleFormSubmit}
-      >
-        <section className="flex justify-between px-1 border-b-2 border-grey-super_light py-3 mx-5">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Add your file name"
-            className="w-2/3 placeholder-grey-medium text-md"
-            onChange={chooseTitle}
-            required
+    <form
+      className="h-72 w-5/7 rounded-2xl bg-white flex flex-col justify-evenly absolute z-50 inset-1/7 md:inset-y-1/4 shadow-xl"
+      onSubmit={handleFormSubmit}
+    >
+      <section className="flex justify-between px-1 border-b-2 border-grey-super_light py-3 mx-5">
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Add your event name"
+          className="w-2/3 placeholder-grey-medium text-md"
+          onChange={chooseTitle}
+        />
+        <button type="button" onClick={handleShowModal}>
+          <Icon iconName="close" />
+        </button>
+      </section>
+      <section className="flex justify-between border-b-2 border-grey-super_light px-1 py-3 mx-5">
+        <div>
+          <DatePicker
+            selectsRange
+            startDate={startDate}
+            endDate={endDate}
+            onChange={(update) => {
+              setDateRange(update)
+            }}
+            placeholderText="Select event dates"
           />
-          <button type="button" onClick={handleShowModal}>
-            <Icon iconName="close" />
-          </button>
-        </section>
-        <section className="flex justify-between border-b-2 border-grey-super_light px-1 py-3 mx-5">
-          <div className="flex justify-end items-center w-2/7">
-            {imgPreview && (
-              <img
-                src={imgPreview}
-                alt="Preview"
-                className="w-10 h-10 rounded-sm mr-2"
-              />
-            )}
-            <div>
-              <button
-                type="button"
-                onClick={handleClick}
-                className="flex items-center"
-              >
-                <span className="text-sm pr-3">
-                  {imgPreview ? "File preview" : "Select your File"}
-                </span>
-                <Icon iconName="add" iconStyle="fill-inactive text-pink" />
-              </button>
-              <input
-                type="file"
-                name="file"
-                size="60"
-                className="hidden"
-                ref={hiddenFileInput}
-                onChange={chooseBannerImage}
-                required
-              />
-            </div>
-          </div>
-        </section>
-        <section className="flex justify-between px-5">
-          <SwitchButton
-            nameLeft="PUBLIC"
-            nameRight="PRIVATE"
-            toogle={() => setPrivacy(!privacy)}
-          />
-          <Button
-            buttonName="Add a File"
-            buttonStyle="btnLibraryStyle"
-            buttonSubmit
-          />
-        </section>
-      </form>
-    </div>
+        </div>
+        <input
+          type="text"
+          placeholder="Select location"
+          onChange={chooseLocation}
+        />
+        <button
+          type="button"
+          onClick={handleClick}
+          className="flex justify-around items-center w-2/7"
+        >
+          <span className="text-sm">Add Cover Image</span>
+          <Icon iconName="add" iconStyle="fill-inactive text-sky" />
+        </button>
+        <input
+          type="file"
+          size="60"
+          className="hidden"
+          ref={hiddenFileInput}
+          onChange={chooseBannerImage}
+        />
+      </section>
+      <section className="flex justify-center border-b-2 border-grey-super_light px-1 py-3 mx-5">
+        <input
+          type="text"
+          placeholder="Briefly explain what your event is about"
+          className="w-full placeholder-grey-medium text-base h-10"
+          onChange={chooseDescription}
+        />
+      </section>
+      <section className="flex justify-between px-5">
+        <SwitchButton
+          nameLeft="PUBLIC"
+          nameRight="PRIVATE"
+          toogle={() => setPrivacy(!privacy)}
+        />
+        <Button
+          buttonName="Create Event"
+          buttonStyle="btnEventStyle"
+          buttonSubmit
+        />
+      </section>
+    </form>
   )
 }
 
--- client/src/components/sections/events/MainEventsContent.js
+++ client/src/components/sections/events/MainEventsContent.js
@@ -1,18 +1,34 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable react/prop-types */
-import React from "react"
+import React, { useState } from "react"
 import EventCards from "../../core/events/EventCards"
 import AddCard from "../../core/AddCard"
+import EventModal from "../../core/events/EventModal"
 
-const MainEventsContent = ({ events }) => (
-  <div className="w-full flex justify-center flex-wrap gap-3 m-6">
-    <button type="button" className="pr-28">
-      <AddCard cardTitle="New event" color="#27A8DF" />
-    </button>
-    {events.map((event) => (
-      <EventCards key={event._id} events={event} />
-    ))}
-  </div>
-)
+const MainEventsContent = ({ events }) => {
+  const [showModal, setShowModal] = useState(false)
+  const handleShowModal = () => {
+    setShowModal(!showModal)
+  }
+
+  return (
+    <div className="relative">
+      {showModal && <EventModal handleShowModal={handleShowModal} />}
+      <div
+        className={`w-full flex justify-evenly flex-wrap gap-3 m-6 filter ${
+          showModal && "blur-md"
+        }`}
+      >
+        <button type="button" className="pr-48" onClick={handleShowModal}>
+          <AddCard cardTitle="New event" color="#27A8DF" />
+        </button>
+        {events.map((event) => (
+          <EventCards key={event._id} events={event} />
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default MainEventsContent
