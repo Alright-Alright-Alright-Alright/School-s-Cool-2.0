d16c26a6e35d8899579c09ed0af8938d2c425c7a
Dirk Kelderman <dirkkelderman@gmail.com>
2021-12-31 19:45:49 +0100

    deploy settings

--- client/src/redux/services/activityService.js
+++ client/src/redux/services/activityService.js
@@ -5,8 +5,7 @@ export const getAllActivityService = () =>
     .get(`/activities`, configHeaders())
     .then((responseFromAPI) => responseFromAPI.data)
 
-export const getFollowedActivityService = () => {
+export const getFollowedActivityService = () =>
   service
     .get(`/followedActivities`, configHeaders())
     .then((responseFromAPI) => responseFromAPI.data)
-}
