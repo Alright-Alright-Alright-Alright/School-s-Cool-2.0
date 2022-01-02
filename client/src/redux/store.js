25c67867b0c0d049d458ca47b6e1fa3a2b7ba86e
Dirk Kelderman <dirkkelderman@gmail.com>
2022-01-02 21:58:45 +0100

    Another bug fix try

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -30,9 +30,9 @@ const store = createStore(
   reducers,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__?.()
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //   window.__REDUX_DEVTOOLS_EXTENSION__?.()
   )
 )
 
