d0109849b35d58f57e32115905412f4622a4022b
In-S4n3 <tiagodcpp@gmail.com>
2022-01-14 14:32:16 +0000

    working on auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { Suspense, lazy, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Route, Routes, Outlet, useNavigate } from "react-router-dom"
+import jwt from "jsonwebtoken"
 import NavBar from "./components/layout/NavBar"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
@@ -16,14 +17,18 @@ function App() {
   const token = localStorage.getItem("Authorization")
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const currentTime = new Date().getTime().toString().slice(0, 10)
 
   useEffect(() => {
-    if (!loggedIn && !token) {
+    if (
+      jwt.decode(token?.slice(7, token.length))?.exp < Number(currentTime) ||
+      (!loggedIn && !token)
+    ) {
       navigate("/login")
     } else {
       dispatch(loggedInUser())
     }
-  }, [dispatch, token, loggedIn, navigate])
+  }, [dispatch, token, loggedIn, navigate, currentTime])
 
   return (
     <div className="App">
