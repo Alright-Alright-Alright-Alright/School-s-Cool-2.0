9b20461e1babea19158f3c94917e2decaca4fa8e
In-S4n3 <tiagodcpp@gmail.com>
2022-01-21 18:11:09 +0000

    files are ordered by numb of likes

--- client/src/components/core/library/LibraryItem.js
+++ client/src/components/core/library/LibraryItem.js
@@ -75,92 +75,98 @@ const libraryItem = ({ library, showModal }) => {
           className="overflow-y-auto flex flex-col w-full scrollBar"
           style={{ height: "60vh" }}
         >
-          {isItprivate().map((item, index) => (
-            <tr
-              key={item._id}
-              onClick={() => selectedRow(item._id, index)}
-              className={`h-16 border-b-2 border-grey-medium_light hover:bg-pink-light hover:shadow-md cursor-pointer flex items-center w-full ${
-                tableRowClicked.includes(item._id) && "bg-pink-light"
-              }`}
-            >
-              <td className="flex w-2/6 h-16 justify-start items-center pl-3">
-                <div>
-                  {item?.fileUrl?.includes("pdf") ? (
-                    <Icon iconName="pdf" iconStyle="fill-inactive" />
-                  ) : (
-                    <Icon iconName="jpg" iconStyle="fill-inactive" />
-                  )}
-                </div>
-                <div className="flex flex-col">
-                  <span className="font-semibold">{item.title}</span>
-                  <span className="text-sm text-grey-medium_light">
-                    {item.subject}
-                  </span>
-                </div>
-              </td>
-              <td className="font-semibold pl-3 w-1/6">{item.category}</td>
-              <td className="text-grey-medium_light pl-3 w-1/6">
-                {dayjs(item.createdAt).fromNow()}
-              </td>
-              <td className="px-3 h-16 flex items-center w-2/6">
-                <div className="flex items-center py-3 w-2/3">
-                  <img
-                    className="w-10 h-10 rounded-full mr-2"
-                    src={`${item?.owner?.imageUrl}`}
-                    alt="profile"
-                  />
-                  <p className="text-base font-semibold">
-                    {item?.owner?.firstName} {item?.owner?.lastName}
-                  </p>
-                </div>
-                <div className="flex justify-around items-center w-1/3">
-                  <div className="w-1/2">
-                    {selected?.includes(item.fileUrl) ? (
-                      <button
-                        type="button"
-                        className="pt-1"
-                        onClick={() =>
-                          setSelected(() => {
-                            const i = selected.indexOf(item.fileUrl)
-
-                            if (i > -1) {
-                              selected.splice(i, 1)
-                            }
-                            return selected
-                          })
-                        }
-                      >
-                        <Icon
-                          iconName="follow"
-                          iconStyle="fill-active text-pink"
-                        />
-                      </button>
+          {isItprivate()
+            .sort((a, b) => b.likedBy.length - a.likedBy.length)
+            .map((item, index) => (
+              <tr
+                key={item._id}
+                onClick={() => selectedRow(item._id, index)}
+                className={`h-16 border-b-2 border-grey-medium_light hover:bg-pink-light hover:shadow-md cursor-pointer flex items-center w-full ${
+                  tableRowClicked.includes(item._id) && "bg-pink-light"
+                }`}
+              >
+                <td className="flex w-2/6 h-16 justify-start items-center pl-3">
+                  <div>
+                    {item?.fileUrl?.includes("pdf") ? (
+                      <Icon iconName="pdf" iconStyle="fill-inactive" />
                     ) : (
-                      <button
-                        type="button"
-                        className="pt-1"
-                        onClick={() => setSelected([...selected, item.fileUrl])}
-                      >
-                        <Icon
-                          iconName="follow"
-                          iconStyle="fill-inactive text-pink"
-                        />
-                      </button>
+                      <Icon iconName="jpg" iconStyle="fill-inactive" />
                     )}
                   </div>
-                  <div className="w-1/2">
-                    <button
-                      type="button"
-                      className="pt-2"
-                      onClick={() => dispatch(deleteFile(item._id))}
-                    >
-                      {user._id === item.owner._id && <Icon iconName="trash" />}
-                    </button>
+                  <div className="flex flex-col">
+                    <span className="font-semibold">{item.title}</span>
+                    <span className="text-sm text-grey-medium_light">
+                      {item.subject}
+                    </span>
                   </div>
-                </div>
-              </td>
-            </tr>
-          ))}
+                </td>
+                <td className="font-semibold pl-3 w-1/6">{item.category}</td>
+                <td className="text-grey-medium_light pl-3 w-1/6">
+                  {dayjs(item.createdAt).fromNow()}
+                </td>
+                <td className="px-3 h-16 flex items-center w-2/6">
+                  <div className="flex items-center py-3 w-2/3">
+                    <img
+                      className="w-10 h-10 rounded-full mr-2"
+                      src={`${item?.owner?.imageUrl}`}
+                      alt="profile"
+                    />
+                    <p className="text-base font-semibold">
+                      {item?.owner?.firstName} {item?.owner?.lastName}
+                    </p>
+                  </div>
+                  <div className="flex justify-around items-center w-1/3">
+                    <div className="w-1/2">
+                      {selected?.includes(item.fileUrl) ? (
+                        <button
+                          type="button"
+                          className="pt-1"
+                          onClick={() =>
+                            setSelected(() => {
+                              const i = selected.indexOf(item.fileUrl)
+
+                              if (i > -1) {
+                                selected.splice(i, 1)
+                              }
+                              return selected
+                            })
+                          }
+                        >
+                          <Icon
+                            iconName="follow"
+                            iconStyle="fill-active text-pink"
+                          />
+                        </button>
+                      ) : (
+                        <button
+                          type="button"
+                          className="pt-1"
+                          onClick={() =>
+                            setSelected([...selected, item.fileUrl])
+                          }
+                        >
+                          <Icon
+                            iconName="follow"
+                            iconStyle="fill-inactive text-pink"
+                          />
+                        </button>
+                      )}
+                    </div>
+                    <div className="w-1/2">
+                      <button
+                        type="button"
+                        className="pt-2"
+                        onClick={() => dispatch(deleteFile(item._id))}
+                      >
+                        {user._id === item.owner._id && (
+                          <Icon iconName="trash" />
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
