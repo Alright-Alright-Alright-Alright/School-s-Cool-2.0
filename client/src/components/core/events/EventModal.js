1dd70312eed51bc7e0e052e47fb60525cd378893
In-S4n3 <tiagodcpp@gmail.com>
2022-01-27 19:43:19 +0000

    Event modal error handler done

--- client/src/components/core/events/EventModal.js
+++ client/src/components/core/events/EventModal.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React, { useState, useRef } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import DatePicker from "react-datepicker"
 import fileUploadHandler from "../../../middleware/UploadFile"
 import { createNewEvent } from "../../../redux/actions/eventActions"
@@ -10,6 +10,7 @@ import Button from "../Button"
 import SwitchButton from "../SwitchButton"
 import Icon from "../Icon"
 import "react-datepicker/dist/react-datepicker.css"
+import MessageHandler from "../MessageHandler"
 
 const Modal = ({ handleShowModal }) => {
   const [title, seTitle] = useState("")
@@ -20,6 +21,7 @@ const Modal = ({ handleShowModal }) => {
   const [dateRange, setDateRange] = useState([null, null])
   const [startDate, endDate] = dateRange
   const hiddenFileInput = useRef(null)
+  const UI = useSelector((state) => state.UI)
 
   const dispatch = useDispatch()
 
@@ -55,7 +57,8 @@ const Modal = ({ handleShowModal }) => {
       bannerImage: image,
       isPrivate: privacy,
     }
-    handleShowModal()
+    if (eventData.title.length > 0 && eventData.location.length > 0)
+      handleShowModal()
     dispatch(createNewEvent(eventData))
   }
 
@@ -65,6 +68,7 @@ const Modal = ({ handleShowModal }) => {
         className="h-72 w-5/7 rounded-2xl bg-white flex flex-col justify-evenly absolute z-50 inset-1/7 md:inset-y-1/4 shadow-xl"
         onSubmit={handleFormSubmit}
       >
+        {UI.errors && <MessageHandler error={UI.errors.data.message} />}
         <section className="flex justify-between px-1 border-b-2 border-grey-super_light py-3 mx-5">
           <input
             type="text"
