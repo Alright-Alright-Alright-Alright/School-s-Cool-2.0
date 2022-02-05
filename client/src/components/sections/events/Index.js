35bf91562ebaa2858702c05e8299ffde7d8defa5
In-S4n3 <tiagodcpp@gmail.com>
2022-02-05 14:55:03 +0000

    merged dev

--- client/src/components/sections/events/Index.js
+++ client/src/components/sections/events/Index.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
+import Calendar from "react-calendar"
+import "react-calendar/dist/Calendar.css"
 import Main from "../../layout/main"
 import MainEventsContent from "./MainEventsContent"
 import { getAllEvents } from "../../../redux/actions/eventActions"
 
 const Index = () => {
   const events = useSelector((state) => state.events.allEvents)
+  const [value, onChange] = useState(new Date())
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -14,7 +17,10 @@ const Index = () => {
 
   return (
     <div>
-      <Main main={<MainEventsContent events={events} />} />
+      <Main
+        main={<MainEventsContent events={events} />}
+        contentLeft={<Calendar onChange={onChange} value={value} />}
+      />
     </div>
   )
 }
