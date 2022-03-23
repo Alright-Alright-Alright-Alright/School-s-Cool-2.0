ab98d950f485027f7a23d592c9082aeacd73c047
Dirk Kelderman <dirkkelderman@gmail.com>
2022-03-23 14:17:14 +0100

    Added loading skeletons for homepage

--- client/src/components/sections/homepage/DashCardsRight.js
+++ client/src/components/sections/homepage/DashCardsRight.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
-// import Dashcard from "../../core/dashcard/Dashcard"
 import TopicDashCard from "../../core/topics/topicDashCard/TopicDashcard"
 import dashcardDropdownMenu from "../../../data/dashcardDropdownMenu.json"
 import { getAllTheUsers } from "../../../redux/actions/userActions"
+import DashcardSkeleton from "../../core/skeleton/DashCardSkeleton"
 
 function DashCardsRight() {
   const allUsers = useSelector((state) => state.user.users)
@@ -12,6 +12,14 @@ function DashCardsRight() {
   useEffect(() => {
     dispatch(getAllTheUsers())
   }, [dispatch])
+
+  if (allUsers.length === 0) {
+    return (
+      <div>
+        <DashcardSkeleton />
+      </div>
+    )
+  }
 
   return (
     <div className="max-w-md px-6 hidden lg:block ">
