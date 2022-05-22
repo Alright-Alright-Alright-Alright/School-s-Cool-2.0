3cfa8509f1cceae3e38383263326a951356c6bb7
Kasper Karelse <kasperkarelse98@gmail.com>
2022-05-22 15:38:03 +0200

    typo

--- client/src/pages/lesson/Summary.js
+++ client/src/pages/lesson/Summary.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 function Summary() {
   return (
     <main className="mt-8 flex flex-col justify-center">
-      <p className="m-auto w-44">Einde van deze cursus!</p>
+      <p className="m-auto w-44">Einde van deze les!</p>
 
       <Link to="/courses" className=" m-auto w-44 mt-24">
         <button
           type="button"
           className="bg-sky text-white rounded-md px-4 py-2 hover:bg-sky-dark"
         >
-          Cursus afronden
+          Les afronden
         </button>
       </Link>
     </main>
