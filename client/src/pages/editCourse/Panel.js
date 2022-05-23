6ffd0f49f9b88a81008c1a2de94ce1770a227b31
Kasper Karelse <kasperkarelse98@gmail.com>
2022-05-23 22:49:15 +0200

    button in editor and centering stuff

--- client/src/pages/editCourse/Panel.js
+++ client/src/pages/editCourse/Panel.js
@@ -1,15 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { SaveIcon, LogoutIcon } from "@heroicons/react/solid";
 
 function Panel(props) {
   const { item } = props;
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <img
         alt="A slide"
         src={item.image}
-        className="max-w-6xl border-2 rounded-md border-sky m-auto"
+        className="border-2 rounded-md border-sky mx-auto"
       />
+      <div className="flex justify-start mt-4 gap-2">
+        <button
+          type="button"
+          className="flex gap-2 bg-sky rounded-md text-white px-4 py-2 items-center hover:shadow-md"
+        >
+          <SaveIcon className="w-5 h-5" />
+          Opslaan
+        </button>
+        <button
+          type="button"
+          className="flex gap-2 bg-yellow rounded-md text-white px-4 py-2 items-center hover:shadow-md"
+        >
+          <LogoutIcon className="w-5 h-5" />
+          Editor afsluiten
+        </button>
+      </div>
     </div>
   );
 }
