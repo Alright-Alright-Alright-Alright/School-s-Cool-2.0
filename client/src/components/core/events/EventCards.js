c221a875d757d7b84537d8bb641b5b47e9eac18e
Dirk Kelderman <dirkkelderman@gmail.com>
2022-05-28 14:38:46 +0200

    fixed locale date on event card

--- client/src/components/core/events/EventCards.js
+++ client/src/components/core/events/EventCards.js
@@ -62,11 +62,15 @@ const EventCards = ({ event }) => {
             <div>
               <div className="flex justify-start">
                 <p className="text-sm pr-3 text-grey-medium_light">
-                  {dayjs(event?.dateStart).format("MMM DD, YYYY")}
+                  {dayjs(event?.dateStart)
+                    .locale(localeFromCookies)
+                    .format("MMM DD, YYYY")}
                 </p>
                 <p className="text-sm text-grey-medium_light">
                   {event.dateStart !== event.dateEnd
-                    ? dayjs(event?.dateEnd).format("MMM DD, YYYY")
+                    ? dayjs(event?.dateEnd)
+                        .locale(localeFromCookies)
+                        .format("MMM DD, YYYY")
                     : null}
                   {/* {dayjs(event?.dateEnd).format("MMM DD, YYYY")} */}
                 </p>
